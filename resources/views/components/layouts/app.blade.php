f45d5d003022f181f6471c4e5f9763350d994c77
johnamba <johnvamba@gmail.com>
2024-10-11 16:29:24 +0800

    laravel 10 to 11

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,10 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
  
         <title>{{ $title ?? 'Page Title' }}</title>
-        @livewireStyles
     </head>
     <body>
         {{ $slot }}
-        @livewireScripts
     </body>
 </html>
